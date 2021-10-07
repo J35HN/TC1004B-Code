97e4a127830917b9314d0c30b3cc15058d598439
Jeshua N <pigihunter@gmail.com>
2021-10-06 19:23:46 -0500

    Workable queries

--- baseDatos/primer.py
+++ baseDatos/primer.py
@@ -1,20 +1,24 @@
 import mysql.connector
+from mysql.connector.fabric import connect
 
 def main():
     db = mysql.connector.connect(
         host = "localhost",
-        user = "",
-        passwd = "",
+        user = "root",
+        passwd = ""
     )
     myCursor = db.cursor()
-    menu = 0
+    menu = 1
     while (menu > 0):
         print("[1]. Querie.\n[0]. Salir.")
-        menu = input("\n")
+        menu = int(input("\n"))
         if (menu == 1):
             consulta = input("Querie: ")
             myCursor.execute(consulta)
+            db.commit()
+            print(consulta + "\n");
         elif (menu == 0):
             print("Adios... :)")
 
-main()
+if __name__ == "__main__":
+    main()
